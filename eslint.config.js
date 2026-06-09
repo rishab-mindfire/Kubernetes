7f82a34a3fec087f8f9e5c669eec60cab81fa394
rishab raj verma <rishabrajv@mindfiresolutions.com>
2026-06-09 13:04:30 +0530

    fix: setup for lint

--- eslint.config.js
+++ eslint.config.js
@@ -1,34 +1,13 @@
 import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
-import nextPlugin from '@next/eslint-plugin-next';
 
 export default tseslint.config(
-  // Core JS Recommended rules
   js.configs.recommended,
-  // Automated TypeScript Configurations
   ...tseslint.configs.recommended,
   {
-    files: ['**/*.{js,jsx,ts,tsx}'],
-    plugins: {
-      '@next/next': nextPlugin,
-    },
+    files: ['src/**/*.{js,ts}'],
     rules: {
-      ...nextPlugin.configs.recommended.rules,
-      ...nextPlugin.configs['core-web-vitals'].rules,
-      'react/react-in-jsx-scope': 'off',
-      'no-console': ['error', { allow: ['warn', 'error'] }],
+      'no-console': 'error',
     },
-  },
-
-  // Global Ignores
-  {
-    ignores: [
-      '.next/',
-      'node_modules/',
-      'dist/',
-      'lib/generated/',
-      'commitlint.config.js',
-      'commitlint.config.cjs',
-    ],
   },
 );
